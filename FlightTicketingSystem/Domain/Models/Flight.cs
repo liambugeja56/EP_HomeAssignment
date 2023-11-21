9ad351b31b9eac12cd62f6d55fef9c32363c75bf
Liam Bugeja <liam.bugeja.f32014@mcast.edu.mt>
2023-11-21 17:28:11 +0100

    KU1.1 & KU3.4 (Architecture and Models)

---
+++ FlightTicketingSystem/Domain/Models/Flight.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Flight
+    {
+        public Guid Id { get; set; }
+
+        public int Rows { get; set; }
+
+        public string Columns { get; set; }
+
+        public DateTime DepartureDate { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+
+        public string CountryFrom { get; set; }
+
+        public string CountryTo { get; set; }
+
+        public double WholesalePrice { get; set; }
+
+        public double CommissionRate { get; set; }
+    }
+}
---
+++ FlightTicketingSystem/Domain/Models/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+
+        public int Row { get; set; }
+
+        public string Column { get; set; }
+
+        public int FlightIdFK { get; set; }
+
+        public string Passport { get; set; }
+
+        public double PricePaid { get; set; }
+
+        public bool Cancelled { get; set; }
+    }
+}
